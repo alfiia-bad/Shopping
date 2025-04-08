85a9439c2d80d81c4ee8bf59546221167a20b2b9
alfiia-bad <136993123+alfiia-bad@users.noreply.github.com>
2025-04-09 01:37:01 +0500

    Порт

--- backend/server.py
+++ backend/server.py
@@ -52,4 +52,5 @@ def send_to_telegram():
         return jsonify({"success": False, "message": "Ошибка при соединении с Telegram"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
