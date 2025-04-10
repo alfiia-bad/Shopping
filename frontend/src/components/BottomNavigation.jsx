1d4d11d300a670fdb53dad1d4428a38a8c333c21
alfiia-bad <136993123+alfiia-bad@users.noreply.github.com>
2025-04-10 12:45:02 +0500

    Create BottomNavigation.jsx

---
+++ frontend/src/components/BottomNavigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { FiShoppingBag, FiHeart, FiBell } from 'react-icons/fi';
+import { FaShoppingCart } from 'react-icons/fa';
+import './BottomNavigation.css';
+
+const BottomNavigation = () => {
+  return (
+    <nav className="bottom-nav">
+      <NavLink to="/" className="nav-item">
+        <FiShoppingBag className="icon" />
+        <span className="label">Товары</span>
+      </NavLink>
+      <NavLink to="/favorites" className="nav-item">
+        <FiHeart className="icon" />
+        <span className="label">Избранное</span>
+      </NavLink>
+      <NavLink to="/cart" className="nav-item">
+        <FaShoppingCart className="icon" />
+        <span className="label">Корзина</span>
+      </NavLink>
+      <NavLink to="/notifications" className="nav-item">
+        <FiBell className="icon" />
+        <span className="label">Уведомления</span>
+      </NavLink>
+    </nav>
+  );
+};
+
+export default BottomNavigation;
