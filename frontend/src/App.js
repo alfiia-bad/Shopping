692976ff393263f8c437c812134995269761ef7c
alfiia-bad <alfiia-bad@mail.com>
2025-04-13 17:12:51 +0500

    смена имени сайта во фронтенде

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,7 +13,7 @@ const products = [
   { id: "3", name: "Кофе", image: "/images/coffee.jpg" },
 ];
 
-const API_URL = "https://alfa-shopping.onrender.com";
+const API_URL = "https://alfa-shop-ljmg.onrender.com";
 
 const App = () => {
   const [cart, setCart] = useState([]);
